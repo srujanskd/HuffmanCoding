ca66d6baaee07722bd2b49459620fe44c213abc5
srujanskd <srujanskd@gmail.com>
2023-02-15 17:02:34 +0530

    Adaptive runner

---
+++ src/main/java/com/compression/huffman/AdaptiveZip.java
@@ -0,0 +1,34 @@
+package com.compression.huffman;
+
+import com.compression.huffman.adaptive.compress.HuffmanCompress;
+import com.compression.huffman.adaptive.decompress.HuffmanDecompress;
+
+import java.io.File;
+
+public class AdaptiveZip {
+    public static void main(String[] args) {
+        Long starTime = System.currentTimeMillis();
+        if(args.length != 3) {
+            System.err.println("Usage: java HuffmanZip -compress input_file output_file");
+            System.err.println("Usage: java HuffmanZip -decompress input_file output_file");
+            System.exit(1);
+            return;
+        }
+        if(args[0].equals("-compress")){
+            File inp = new File(args[1]);
+            File out = new File((args[2]));
+            HuffmanCompress huffComp = new HuffmanCompress();
+            huffComp.compressFile(inp, out);
+        }
+        else if(args[0].equals("-decompress")) {
+            File inp = new File(args[1]);
+            File out = new File((args[2]));
+            HuffmanDecompress huffDecomp = new HuffmanDecompress();
+            huffDecomp.decompressFile(inp, out);
+        }
+
+        Long endTime = System.currentTimeMillis();
+        System.out.println("Execution time : " + (endTime - starTime) + "ms");
+
+    }
+}
